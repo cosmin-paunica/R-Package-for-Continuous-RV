bcfa93626e5a52c6d0756c9d2a1b911ed2b83a83
Cosmin Păunică <cosmin_paunica@yahoo.com>
2021-02-03 11:56:51 +0200

    functions for (central) moment

--- main.R
+++ main.R
@@ -1,20 +1,17 @@
 # 1
-normalizing_constant <- function(f)
-{
+normConstant <- function(f) {
   I = integrate(f, -Inf, Inf)$value
   return(1/I)
 }
 
-# f <- function(x) {exp((-x**2))}
-I = normalizing_constant(f)
-
 # 2
 allValuesPos <- function(vec) all(vec >= 0)
-verifyPositive <- function(fAux){
+
+verifyPositive <- function(fAux) {
   allValuesPos(fAux(seq(-100, 100, 0.1)))
 }
-is_prob_density <- function(faux)
-{
+
+isProbDensity <- function(faux) {
   if(!verifyPositive(faux))
     F
   if (round(integrate(faux, -Inf, Inf)$value, 4) == 1)
@@ -23,11 +20,10 @@ is_prob_density <- function(faux)
     F
 }
 
-
 #3
-CRV <- function(pdFuncion,type=1){
-  if(!is_prob_density(pdFuncion))
-    return(FALSE)
+CRV <- function(pdFuncion,type=1) {
+  if(!isProbDensity(pdFuncion))
+    stop("function is not a probability density")
   object = pdFuncion
   attr(object,"pdf") = pdFuncion 
   attr(object,"type")= type
@@ -35,39 +31,57 @@ CRV <- function(pdFuncion,type=1){
   return(object)
 }
 
-i=is_prob_density(dnorm)
-test <- CRV(dnorm)
-class(test)
-integrate(dnorm,-Inf,Inf)$value
-
-
-expectedValue <- function(X){#intoarce valoare medie a distributiei
+# 5
+expectedValue <- function(X) { # intoarce valoare medie a distributiei
   if(class(X)!="CRV")
-    return()
+    stop("X is not a continuous random variable")
   functionFromX<- attr(X,"pdf")
   integrate(function(x){x*functionFromX(x)},-Inf,Inf)$value
 }
 
-meanOfFunction <- function(g, X){ #intoarce valoarea medie a lui g(X)
+varianceValue<-function(X) {
   if(class(X)!="CRV")
-    return()
+    stop("X is not a continuous random variable")
   functionFromX<- attr(X,"pdf")
-  integrate(function(x){x*g(functionFromX(x))},-Inf,Inf)$value
-}
-
-varianceValue<-function(X){
-  if(class(X)!="CRV")
-    return()
-  functionFromX<- attr(X,"pdf")
-  toVariance <- function(x){
+  toVariance <- function(x) {
     (x^2)*functionFromX(x)
   }
   integrate(toVariance,-Inf,Inf)$value - (expectedValue(X)^2)
 }
 
+moment <- function(X, k) {
+  if (k < 1 || k > 4) {
+    stop("k nu este corespunzator")
+  }
+  if (class(X) != "CRV") {
+    stop("X nu este v.a. continua")
+  }
+  meanOfFunction(function(x) {x**k}, X)
+}
+
+centeredMoment <- function(X, k) {
+  if (k < 1 || k > 4) {
+    stop("k nu este corespunzator")
+  }
+  if (class(X) != "CRV") {
+    stop("X nu este v.a. continua")
+  }
+  functionFromX <- attr(X,"pdf")
+  E = expectedValue(X)
+  meanOfFunction(function(x) {(x-E)**k}, X)
+}
+
+# 6
+meanOfFunction <- function(g, X){ # intoarce valoarea medie a lui g(X)
+  if(class(X)!="CRV")
+    stop("X is not a continuous random variable")
+  functionFromX<- attr(X,"pdf")
+  integrate(function(x){x*g(functionFromX(x))},-Inf,Inf)$value
+}
+
 varianceOfFunction <- function(g,X){
   if(class(X)!="CRV")
-    return()
+    stop("X is not a continuous random variable")
   functionFromX<- attr(X,"pdf")
   toVariance <- function(x){
     (x^2)*g(functionFromX(x))
@@ -75,8 +89,9 @@ varianceOfFunction <- function(g,X){
   integrate(toVariance,-Inf,Inf)$value - (expectedValue(X)^2)
 }
 
-Pcont<-function(continua,a=-Inf,b){
-  if(!class(continua)=="CRV")
-    return(1)
-  integrate(attr(continua,"pdf"),a,b)
+# 7
+PCont<-function(X,a=-Inf,b){
+  if(!class(X)=="CRV")
+    stop("X is not a continuous random variable")
+  integrate(attr(X,"pdf"),a,b)
 }
